4b8f9454bf91bec28ad6f84f5cb53783f2c95aaf
bala-amypo <bala@amypo.in>
2025-12-19 06:40:18 +0000

    User commit

--- src/main/java/com/example/demo/Servicesimpl/StudentServiceImple.java
+++ src/main/java/com/example/demo/Servicesimpl/StudentServiceImple.java
@@ -1,4 +1,4 @@
-package com.example.demo.Serviceimpl;
+package com.example.demo.serviceimpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -6,14 +6,14 @@ import org.springframework.stereotype.Service;
 import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRespository;
 import com.example.demo.service.StudentService;
-
+import com.example.demo.exception.ResourceNotFoundException;
 
 @Service
 
 public class StudentServiceImple implements StudentService {
-    // private final StudentRespository studentRespository;
-    // public StudentServiceImple(StudentRespository studentRespository){
-     //   this.studentRespository=studentRespository;
+    // private final StudentRespository studentRepository;
+    // public StudentServiceImple(StudentRespository studentRepository){
+     //   this.studentRespoitory=studentRepository;
     // }
     @Autowired
     StudentRespository studentRespository;
@@ -22,4 +22,19 @@ public class StudentServiceImple implements StudentService {
 
         return studentRespository.save(student);
     }
+    public Student getStudentById(Long id){
+        return studentRespository.findById(id)
+        .orElseThrow(()-> new ResourceNotFoundException("Student not found"));
+    }
+    public Student updatedata(Long id, Student student) {
+        Student exits=getStudentById(id);
+        exits.setName(student.getName());
+        exits.setEmail(student.getEmail());
+        return studentRespository.save(exits);
+        // orElseThrow(()-> new ResourceNotFoundException( "Student not found"));
+    } 
+    public void deletedata(Long id) {
+        Student student=getStudentById(id);
+            studentRespository.delete(student);
+    } 
 }
